021279cc40bd3d64e291bbe3dfe1bb36af57af98
Stephan Gabler <stephan.gabler@gmail.com>
2011-04-12 10:53:58 +0200

    first running version with depth image

--- main.cpp
+++ main.cpp
@@ -1,82 +1,86 @@
+
+
 /*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2011, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *	
- * Author: Nico Blodow (blodow@cs.tum.edu)
+    help for writing this code came from the following sites:
+    http://www.ros.org/wiki/pcl/Tutorials/Range%20image%20visualization
+    http://www.ros.org/wiki/pcl/Tutorials/Range%20image%20creation#Tutorial_code
+    http://pointclouds.org/documentation/tutorials/kinect_grabber.php
+    http://www.ros.org/wiki/pcl_ros/Tutorials/CloudToImage
  */
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/kinect_grabber.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/range_image_visualizer.h>
+#include <Eigen/Geometry>
+#include "pcl/range_image/range_image.h"
 
-class SimpleKinectViewer
+
+class SimpleRangeViewer
 {
   public:
-    SimpleKinectViewer () : viewer ("PCL Kinect Viewer") {}
 
-    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
+    // constructor
+    SimpleRangeViewer () : cloud_viewer("Cloud Viewer"),
+                            range_viewer("Range Viewer"){}
+
+
+
+    // the function which is called regularly for visualization
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
     {
-      if (!viewer.wasStopped())
-        viewer.showCloud (*cloud);
+
+        pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::CAMERA_FRAME;
+        float angularResolution     =   1.0f * (M_PI/180.0f); //   1.0 degree in rad
+        float maxAngleWidth         = 360.0f * (M_PI/180.0f); // 360.0 degree in rad
+        float maxAngleHeight        = 180.0f * (M_PI/180.0f); // 180.0 degree in rad
+        Eigen::Affine3f sensorPose  = (Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f);
+        float noiseLevel            = 0.00;
+        float minRange              = 0.0f;
+        int borderSize              = 0;
+
+        if (!cloud_viewer.wasStopped())
+          cloud_viewer.showCloud(*cloud);
+
+        pcl::RangeImage rangeImage;
+        rangeImage.createFromPointCloud(*cloud, angularResolution, maxAngleWidth, maxAngleHeight,
+                                        sensorPose, coordinate_frame, noiseLevel, minRange, borderSize);
+        range_viewer.setRangeImage(rangeImage, 0., 1000., true);
     }
     
+
+    // the function which is called to run the class
     void run ()
     {
-      pcl::Grabber* interface = new pcl::OpenNIGrabber();
 
-      boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f = 
-        boost::bind (&SimpleKinectViewer::cloud_cb_, this, _1);
+        // interface to the NI (from here we read the pointcloud
+        pcl::Grabber* interface = new pcl::OpenNIGrabber();
+
+        boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
+            boost::bind (&SimpleRangeViewer::cloud_cb_, this, _1);
 
       boost::signals2::connection c = interface->registerCallback (f);
       
-      interface->start ();
-      
-      while (!viewer.wasStopped())
+      interface->start ();      
+      while(!cloud_viewer.wasStopped() || range_viewer.isShown())
       {
-        sleep (1);
+          pcl_visualization::ImageWidgetWX::spinOnce();  // process GUI events
+          usleep(100000);
       }
-
       interface->stop ();
     }
 
-    pcl_visualization::CloudViewer viewer;
+
+    // class members
+    pcl_visualization::RangeImageVisualizer range_viewer;
+    pcl_visualization::CloudViewer cloud_viewer;
+
 };
 
 int main ()
 {
-  SimpleKinectViewer v;
+  SimpleRangeViewer v;
   v.run ();
   return 0;
 }
-
-
