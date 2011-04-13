645740d8d08cb30aedf7528b94ff20f9328dcd06
dedan <dedan@ubuntu.(none)>
2011-04-13 11:51:31 +0200

    pointcloud and object_border visualisation.
    it works but is very slow and always segfaults after 10 to 30 seconds

--- main.cpp
+++ main.cpp
@@ -19,69 +19,85 @@
 
 class SimpleRangeViewer
 {
-  public:
+private:
 
-    // constructor
-    SimpleRangeViewer () : range_viewer("Range Viewer"){}
+    // class members
+    pcl_visualization::RangeImageVisualizer range_viewer;
+    pcl_visualization::CloudViewer cloud_viewer;
+    pcl::RangeImage::CoordinateFrame coordinate_frame;
+    float angularResolution;
+    float maxAngleWidth;
+    float maxAngleHeight;
+    Eigen::Affine3f sensorPose;
+    float noiseLevel;
+    float minRange;
+    int borderSize;
+
+public:
+
+    // constructor - initialization
+    SimpleRangeViewer () :  cloud_viewer("cloud"),
+                            range_viewer("Range Viewer"),
+                            coordinate_frame(pcl::RangeImage::CAMERA_FRAME),
+                            angularResolution(pcl::deg2rad(1.0f)),
+                            maxAngleWidth(pcl::deg2rad(360.0f)),
+                            maxAngleHeight(pcl::deg2rad(180.0f)),
+                            sensorPose((Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f)),
+                            noiseLevel(0.00),
+                            minRange(0.0f),
+                            borderSize(0){}
 
 
-    // the function which is called regularly for visualization
+    // visualize the cloud
+    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
+    {
+        if (!cloud_viewer.wasStopped())
+            cloud_viewer.showCloud(*cloud);
+    }
+
+
+    // visualize the depth image with borders
     void range_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
     {
-
-        pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::CAMERA_FRAME;
-        float angularResolution     = pcl::deg2rad(1.0f);
-        float maxAngleWidth         = pcl::deg2rad(360.0f);
-        float maxAngleHeight        = pcl::deg2rad(180.0f);
-        Eigen::Affine3f sensorPose  = (Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f);
-        float noiseLevel            = 0.00;
-        float minRange              = 0.0f;
-        int borderSize              = 0;
-
         pcl::RangeImage range_image;
         range_image.createFromPointCloud(*cloud, angularResolution, maxAngleWidth, maxAngleHeight,
-                                        sensorPose, coordinate_frame, noiseLevel, minRange, borderSize);
+                                         sensorPose, coordinate_frame, noiseLevel, minRange, borderSize);
         pcl::RangeImageBorderExtractor border_extractor(&range_image);
         pcl::PointCloud<pcl::BorderDescription> border_descriptions;
         border_extractor.compute(border_descriptions);
 
-        range_viewer.getRangeImageBordersWidget(range_image, -INFINITY, INFINITY, false,
-                                                border_descriptions, "Range image with borders");
-        range_viewer.setRangeImage(range_image, 0., 1000., true);
+        range_viewer.visualizeBorders(range_image, -INFINITY, INFINITY, false, border_descriptions);
     }
-
 
 
     // the function which is called to run the class
     void run ()
     {
-
         // interface to the NI (from here we read the pointcloud
         pcl::Grabber* interface = new pcl::OpenNIGrabber();
 
         boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> g =
-            boost::bind (&SimpleRangeViewer::range_cb_, this, _1);
+                boost::bind (&SimpleRangeViewer::range_cb_, this, _1);
+        boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f =
+                boost::bind (&SimpleRangeViewer::cloud_cb_, this, _1);
 
-        interface->registerCallback (g);
+        interface->registerCallback(g);
+        interface->registerCallback(f);
+
         interface->start();
-        while(true)
+        while(!cloud_viewer.wasStopped() || range_viewer.isShown())
         {
             pcl_visualization::ImageWidgetWX::spinOnce();  // process GUI events
             usleep(100000);
         }
         interface->stop ();
     }
-
-
-    // class members
-    pcl_visualization::RangeImageVisualizer range_viewer;
-//    pcl_visualization::CloudViewer cloud_viewer;
-
 };
+
 
 int main ()
 {
-  SimpleRangeViewer v;
-  v.run ();
-  return 0;
+    SimpleRangeViewer v;
+    v.run ();
+    return 0;
 }
