355b0a06ba6c2420c1323697fa8fbe75b4eb08d3
Stephan Gabler <stephan.gabler@gmail.com>
2011-04-20 18:31:24 +0200

    changed main to use only openni and openCV
    realized that we don't need pcl and that kinect is now supported in openCV.
    
    this is a first simple program which computes sift features for a specified
    region of interest.

--- main.cpp
+++ main.cpp
@@ -1,103 +1,114 @@
 
 
-/*
-    help for writing this code came from the following sites:
-    http://www.ros.org/wiki/pcl/Tutorials/Range%20image%20visualization
-    http://www.ros.org/wiki/pcl/Tutorials/Range%20image%20creation#Tutorial_code
-    http://pointclouds.org/documentation/tutorials/kinect_grabber.php
- */
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include "siftfast.h"
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/io/kinect_grabber.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/visualization/range_image_visualizer.h>
-#include <Eigen/Geometry>
-#include "pcl/range_image/range_image.h"
-#include <pcl/features/range_image_border_extractor.h>
+using namespace cv;
+using namespace std;
 
 
-class SimpleRangeViewer
+Image create_sift_patch(Mat image, Point p1, Point p2)
 {
-private:
-
-    // class members
-    pcl_visualization::RangeImageVisualizer range_viewer;
-    pcl_visualization::CloudViewer cloud_viewer;
-    pcl::RangeImage::CoordinateFrame coordinate_frame;
-    float angularResolution;
-    float maxAngleWidth;
-    float maxAngleHeight;
-    Eigen::Affine3f sensorPose;
-    float noiseLevel;
-    float minRange;
-    int borderSize;
-
-public:
-
-    // constructor - initialization
-    SimpleRangeViewer () :  cloud_viewer("cloud"),
-                            range_viewer("Range Viewer"),
-                            coordinate_frame(pcl::RangeImage::CAMERA_FRAME),
-                            angularResolution(pcl::deg2rad(1.0f)),
-                            maxAngleWidth(pcl::deg2rad(360.0f)),
-                            maxAngleHeight(pcl::deg2rad(180.0f)),
-                            sensorPose((Eigen::Affine3f)Eigen::Translation3f(0.0f, 0.0f, 0.0f)),
-                            noiseLevel(0.00),
-                            minRange(0.0f),
-                            borderSize(0){}
-
-
-    // visualize the cloud
-    void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
+    assert(p2.y > p1.y);
+    assert(p2.x > p1.x);
+    int height = p2.y - p1.y;
+    int width = p2.x - p1.x;
+    Image patch = CreateImage(height, width);
+    for(int r = 0; r < height; ++r)
     {
-        if (!cloud_viewer.wasStopped())
-            cloud_viewer.showCloud(*cloud);
-    }
-
-
-    // visualize the depth image with borders
-    void range_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
-    {
-        pcl::RangeImage range_image;
-        range_image.createFromPointCloud(*cloud, angularResolution, maxAngleWidth, maxAngleHeight,
-                                         sensorPose, coordinate_frame, noiseLevel, minRange, borderSize);
-        pcl::RangeImageBorderExtractor border_extractor(&range_image);
-        pcl::PointCloud<pcl::BorderDescription> border_descriptions;
-        border_extractor.compute(border_descriptions);
-
-        range_viewer.visualizeBorders(range_image, -INFINITY, INFINITY, false, border_descriptions);
-    }
-
-
-    // the function which is called to run the class
-    void run ()
-    {
-        // interface to the NI (from here we read the pointcloud
-        pcl::Grabber* interface = new pcl::OpenNIGrabber();
-
-        boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> g =
-                boost::bind (&SimpleRangeViewer::range_cb_, this, _1);
-        boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f =
-                boost::bind (&SimpleRangeViewer::cloud_cb_, this, _1);
-
-        interface->registerCallback(g);
-        interface->registerCallback(f);
-
-        interface->start();
-        while(!cloud_viewer.wasStopped() || range_viewer.isShown())
+        for(int c = 0; c < width; ++c)
         {
-            pcl_visualization::ImageWidgetWX::spinOnce();  // process GUI events
-            usleep(100000);
+
+           patch->pixels[r*patch->stride+c] = image.at<uchar>(p1.y + r, p1.x + c) / 255.;
         }
-        interface->stop ();
     }
-};
-
-
-int main ()
-{
-    SimpleRangeViewer v;
-    v.run ();
-    return 0;
+    return patch;
 }
+
+
+int main()
+{
+
+    cout << "Kinect opening ..." << endl;
+    VideoCapture capture( CV_CAP_OPENNI );
+    cout << "done." << endl;
+
+    if( !capture.isOpened() )
+    {
+        cout << "Can not open a capture object." << endl;
+        return -1;
+    }
+
+    capture.set( CV_CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, CV_CAP_OPENNI_VGA_30HZ ); // default
+
+    // Print some avalible Kinect settings.
+    cout << "\nDepth generator output mode:" << endl <<
+            "FRAME_WIDTH    " << capture.get( CV_CAP_PROP_FRAME_WIDTH ) << endl <<
+            "FRAME_HEIGHT   " << capture.get( CV_CAP_PROP_FRAME_HEIGHT ) << endl <<
+            "FRAME_MAX_DEPTH    " << capture.get( CV_CAP_PROP_OPENNI_FRAME_MAX_DEPTH ) << " mm" << endl <<
+            "FPS    " << capture.get( CV_CAP_PROP_FPS ) << endl;
+
+    cout << "\nImage generator output mode:" << endl <<
+            "FRAME_WIDTH    " << capture.get( CV_CAP_OPENNI_IMAGE_GENERATOR+CV_CAP_PROP_FRAME_WIDTH ) << endl <<
+            "FRAME_HEIGHT   " << capture.get( CV_CAP_OPENNI_IMAGE_GENERATOR+CV_CAP_PROP_FRAME_HEIGHT ) << endl <<
+            "FPS    " << capture.get( CV_CAP_OPENNI_IMAGE_GENERATOR+CV_CAP_PROP_FPS ) << endl;
+
+    for(;;)
+    {
+        Mat depthMap;
+        Mat image;
+
+        if( !capture.grab() )
+        {
+            cout << "Can not grab images." << endl;
+            return -1;
+        }
+        else
+        {
+            if(capture.retrieve( depthMap, CV_CAP_OPENNI_DEPTH_MAP ) )
+            {
+                const float scaleFactor = 0.05f;
+                Mat show;
+                depthMap.convertTo( show, CV_8UC1, scaleFactor );
+                imshow( "depth map", show );
+            }
+
+            if(capture.retrieve( image, CV_CAP_OPENNI_GRAY_IMAGE ) )
+            {
+
+
+                Point p1 = Point(10, 10);
+                Point p2 = Point(100, 100);
+                Image img_sift = create_sift_patch(image, p1, p2);
+                Keypoint keypts = GetKeypoints(img_sift);
+                Keypoint key = keypts;
+                while(key)
+                {
+                    rectangle(image,
+                                Point(p1.x + key->col-1 , p1.y + key->row-1),
+                                Point(p1.x + key->col+1 , p1.y + key->row+1),
+                                Scalar(255,0,0), 1);
+                    key = key->next;
+                }
+                rectangle(image, p1, p2, Scalar(80, 0, 0), 1);
+
+                FreeKeypoints(keypts);
+                DestroyAllResources();
+                imshow( "rgb image", image);
+
+            }
+
+            if( waitKey( 30 ) >= 0 )
+                break;
+        }
+    }
+
+    return 0;
+    cvDestroyAllWindows();
+}
+
+
+// TODO depth map maske benutzen
+
