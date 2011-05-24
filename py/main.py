be7592ff8a4eb47d2b15e6e06bf6e40487de815f
Stephan Gabler <stephan.gabler@gmail.com>
2011-05-24 15:10:30 +0200

    cleaned code and added save pictures functionality
    moved all constants to the top, changed order of initializations, etc

--- py/main.py
+++ py/main.py
@@ -22,6 +22,10 @@ k_width = 5
 perc = 0.2
 max_dist = 4000
 n_colors = 100
+cont_length = 50
+cont_area = 500
+n_sift = 20
+n_forget = -3
 
 # create a list of random colors
 color_tab = [random_color() for i in range(n_colors)]
@@ -40,69 +44,63 @@ try:
     save_count = 0
     objects = []
     current_key = -1
+    timing = {'t_draw':0, 't_histo':0, 't_track':0, 't_pre':0}
+    loop_c = 0
+    font = cv.InitFont(cv.CV_FONT_HERSHEY_PLAIN, 1, 1, 0, 1, 8) #Creates a font
+    elem = cv.CreateStructuringElementEx(8, 8, 4, 4, cv.CV_SHAPE_RECT)
+    storage = cv.CreateMemStorage(0)
+    
+    # the image generators
     image_generator = onipy.OpenNIImageGenerator()
-    return_code = g_context.FindExistingNode(onipy.XN_NODE_TYPE_IMAGE, image_generator)
-    current_image_frame = cv.CreateImageHeader(image_generator.Res(), cv.IPL_DEPTH_8U, 3)
-
+    g_context.FindExistingNode(onipy.XN_NODE_TYPE_IMAGE, image_generator)
     depth_generator = onipy.OpenNIDepthGenerator()
-    return_code = g_context.FindExistingNode(onipy.XN_NODE_TYPE_DEPTH, depth_generator)
+    g_context.FindExistingNode(onipy.XN_NODE_TYPE_DEPTH, depth_generator)
     width = depth_generator.XRes()
     height = depth_generator.YRes()
+
+    # matrix headers and matrices for computation buffers    
+    current_image_frame = cv.CreateImageHeader(image_generator.Res(), cv.IPL_DEPTH_8U, 3)
     current_depth_frame = cv.CreateMatHeader(height, width, cv.CV_16UC1)
     for_thresh = cv.CreateMat(height, width, cv.CV_32FC1)
+    min_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
+    max_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
+    and_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
+    gray = cv.CreateMat(height, width, cv.CV_8UC1)
+    obj_draw = np.zeros((height, width))
+    cont_draw = np.zeros((height, width))
     
     # create some matrices for drawing
     hist_img = cv.CreateMat(hist_height, width, cv.CV_8UC3)
     out = cv.CreateMat(height + hist_height, width, cv.CV_8UC3)
     contours = cv.CreateMat(height, width, cv.CV_8UC3)
-    min_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
-    max_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
-    and_thresh = cv.CreateMat(height, width, cv.CV_8UC1)
-    gray = cv.CreateMat(height, width, cv.CV_8UC1)
-
-    obj_draw = np.zeros((height, width))
-    cont_draw = np.zeros((height, width))
-
-    timing = {'t_init':0, 't_histo':0, 't_track':0, 't_pre':0}
-    loop_c = 0
-    
-    font = cv.InitFont(cv.CV_FONT_HERSHEY_PLAIN, 1, 1, 0, 1, 8) #Creates a font
-    elem = cv.CreateStructuringElementEx(8, 8, 4, 4, cv.CV_SHAPE_RECT)
-    storage = cv.CreateMemStorage(0)
-
-
-
 
     while True:
 
         return_code = g_context.WaitAndUpdateAll()
 
         # get the images
+        # TODO: ueber frames mitteln (entweder nur im histogramm, vielleicht aber auch ueber ganze frames (mit discount factor)
         depth_data_raw = depth_generator.GetGrayscale16DepthMapRaw()
         cv.SetData(current_depth_frame, depth_data_raw)
+        cv.Convert(current_depth_frame, for_thresh)
         image_data_raw = image_generator.GetBGR24ImageMapRaw()
         cv.SetData(current_image_frame, image_data_raw)
         
+        # initialize matrices for drawing and start timing
         t0 = time.time()
-        
-        # initialize matrices
-        depth = np.asarray(current_depth_frame)
         cv.SetZero(hist_img)
         cv.SetZero(out)
         cv.SetZero(contours)
         
-        timing['t_init'] += time.time() - t0
-    
-        # compute and smooth histogram      
+        # compute and smooth histogram
+        depth = np.asarray(current_depth_frame)
         hist, _ = np.histogram(depth, n_bins, range=(min_range, max_range), normed=False)
         hist = np.convolve(hist, np.ones(k_width) / k_width, 'same')
         max_hist = np.max(hist)
-        
         timing['t_histo'] += time.time() - t0
 
         # histogram clustering
-        start = 0
-        end = 0
+        start, end = 0, 0
         c = 1
         conts_list = []
 
@@ -118,7 +116,6 @@ try:
             if ((cur_value < perc * hist[end]) & (1.1 * cur_value < next_value)):
 
                 # cut out a certain depth layer                
-                cv.Convert(current_depth_frame, for_thresh)
                 cv.Threshold(for_thresh, min_thresh, start*10, 255, cv.CV_THRESH_BINARY)
                 cv.Threshold(for_thresh, max_thresh, i*10, 255, cv.CV_THRESH_BINARY_INV)
                 cv.And(min_thresh, max_thresh, and_thresh)
@@ -129,14 +126,13 @@ try:
                 
                 # collect all interesting contours in a list
                 while conts:
-                    if len(conts) > 50 and cv.ContourArea(conts) > 500:
+                    if len(conts) > cont_length and cv.ContourArea(conts) > cont_area:
                         conts_list.append(list(conts))
                     conts = conts.h_next()
                 
                 # prepare for search for next hill
-                start = i
-                end = i
-                c = c + 1
+                start, end = i, i
+                c += 1
                 
                
             # draw current value in histogram
@@ -145,17 +141,18 @@ try:
                    (i, int(hist_height-next_value*hist_height/max_hist)),
                    (i, int(hist_height-cur_value*hist_height/max_hist))]
             cv.FillConvexPoly(hist_img, pts, color_tab[c])
-            
+          
+        # time the histogram clustering  
         timing['t_pre'] += time.time() - t0
 
-
-
-                
-        # pickle the contours
-        if current_key == ord('c'):
-            with open('/home/dedan/obj_att/out/conts_%d.pickle' % save_count, 'w') as f:
-                pickle.dump(conts_list, f)
-                save_count = save_count +1
+        # pickle the contours and save images
+        if current_key == ord('s'):
+            cv.SaveImage('../out/image_%d.jpg' % save_count, current_image_frame)
+            cv.SaveImage('../out/depth_%d.jpg' % save_count, current_depth_frame)            
+            with open('../out/objects_%d.pickle' % save_count, 'w') as f:
+                pickle.dump(objects, f)               
+            save_count = save_count +1
+        
         
         # iterate over tracked objects
         sift = False
@@ -197,39 +194,30 @@ try:
                     found = True
                     conts_list.remove(cont)     
                     break
+
             if not found:
                 if obj.count > 0:
                     obj.count = 0               # once not seen -> immediately on delete list
                 else:
-                    obj.count = obj.count -1    # few times not seen -> it becomes only worse
+                    obj.count -= 1              # few times not seen -> it becomes only worse
         
         # new objects get a chance in the object list
         for cont in conts_list: 
             objects.append(Obj(cont))
+        timing['t_track'] += time.time() - t0
 
-        # print the contours and a box around them
+
+        # draw the contours and a box around them
         for obj in objects:
             if obj.count < 0:
                 continue
             cv.FillPoly(contours, [obj.cont], obj.color)
             if obj.frames != None:
-                col = cv.Scalar(0,0,255)
-            else:
                 col = cv.Scalar(0,255,0)
+            else:
+                col = cv.Scalar(0,0,255)
             for j in range(4):
                 cv.Line(contours, obj.box_points[j], obj.box_points[(j+1)%4], col)
-          
-                
-        
-        
-        # TODO: ueber frames mitteln (entweder nur im histogramm, vielleicht aber auch ueber ganze frames (mit discount factor)
-        
-                
-                
-        timing['t_track'] += time.time() - t0
-
-
-
 
         # output images
         outroi = (0, hist_height, width, height)
@@ -239,6 +227,8 @@ try:
         a = cv.GetSubRect(out, histroi)
         cv.Copy(hist_img, a)
         loop_c +=1
+        timing['t_draw'] += time.time() - t0
+        
         
         i = 0
         for key, val in timing.iteritems():
@@ -254,14 +244,13 @@ try:
         cv.ShowImage( "Image Stream", current_image_frame )
         cv.ShowImage( "Depth Stream", current_depth_frame)
         cv.ShowImage("conts", out)
-
+        
         # wait for user input (keys)
         current_key = cv.WaitKey( 5 ) % 0x100
         if current_key == KEY_ESC:
-            for key, val in timing.iteritems():
-                print "%s: %f" % (key, val / loop_c)
             break
 
 finally:
-
+    for key, val in timing.iteritems():
+        print "%s: %f" % (key, val / loop_c)
     g_context.Shutdown()
