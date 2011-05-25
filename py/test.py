cb316e03cea3e0bad23109de73a79c630f0a0be7
Stephan Gabler <stephan.gabler@gmail.com>
2011-05-25 17:55:13 +0200

    implemented a simple flann test in test.py

--- py/test.py
+++ py/test.py
@@ -1,49 +1,15 @@
 
+import pyflann
+import pickle
+import numpy as np
 
-import cv
+db = pickle.load(open('../out/pickled.db'))
 
-bla = cv.CreateMat(100, 100, cv.CV_8UC1)
+flann = pyflann.FLANN()
+bla = flann.build_index(db['features'][:,:-1])
+print bla
 
-data = {'pt1': None, 'pt2': None, 'draw': True}
-
-def mouse_call(event, x, y, flags, param):
-    
-    if param['draw']:
-        param['pt2'] = (x, y)
-    
-    if event == cv.CV_EVENT_LBUTTONDOWN:
-        param['pt1'] = (x, y)
-        param['pt2'] = None
-        param['draw'] = True
-
-    if event == cv.CV_EVENT_LBUTTONUP:
-        param['draw'] = False
-
-
-cv.NamedWindow('test')
-cv.SetMouseCallback('test', mouse_call, data)
-
-   
-while True:
-    cv.SetZero(bla)
-    if data['pt1'] != None and data['pt2'] != None:
-        cv.Rectangle(bla, data['pt1'], data['pt2'], cv.Scalar(255))
-
-    cv.ShowImage('test', bla)
-
-    current_key = cv.WaitKey( 5 ) % 0x100
-    if current_key == 27:
-        break
-
-
-
-
-#import pyflann
-#from numpy import *
-#from numpy.random import *
-#dataset = rand(10000, 128)
-#testset = rand(1000, 128)
-#flann = pyflann.FLANN()
-#result,dists = flann.nn(dataset,testset,5,algorithm="kmeans", branching=32, iterations=7, checks=16);
-#print result
-#print dists
+tmp = db['features'][15:17,:-1]
+result, dists = flann.nn_index(tmp, 1, checks=bla['checks'])
+print result
+print dists
